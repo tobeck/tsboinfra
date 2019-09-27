5814d4363cdd516aec17b599c00c41a4c0aace54
tobeck <tobias.becker.olsson@gmail.com>
2019-09-27 08:31:46 +0200

    Renamed the admin project for tsbo

--- gcloud/tsbo-admin/project.tf
+++ gcloud/tsbo-admin/project.tf
@@ -3,7 +3,7 @@ provider "google" {
 }
 
 data "google_billing_account" "billing_account" {
-  billing_account = "012242-C85D66-4DCB94"
+  billing_account = "${var.billing_account}"
   open            = true
 }
 
@@ -12,8 +12,8 @@ data "google_organization" "organization" {
 }
 
 resource "google_project" "project" {
-  name       = "tsbo-terraform-admin"
-  project_id = "tsbo-terraform-admin"
+  name       = "tsbo-admin"
+  project_id = "tsbo-admin"
   org_id     = "${data.google_organization.organization.id}"
 
   billing_account = "${data.google_billing_account.billing_account.id}"
---
+++ gcloud/tsbo-admin/variables.tf
@@ -0,0 +1,2 @@
+variable "billing_account" {
+}
