24943aea7b9d1c8c506a639a05f322f29de5edf6
tobeck <tobias.becker.olsson@gmail.com>
2019-10-01 13:08:10 +0200

    Reference the google project id for the dns managed zone

--- gcloud/tsbo-admin/dns.tf
+++ gcloud/tsbo-admin/dns.tf
@@ -2,4 +2,5 @@ resource "google_dns_managed_zone" "tsbo-zone" {
   name        = "tsbo-zone"
   dns_name    = "tsbo.me."
   description = "The tsbo DNS zone"
+  project     = "${google_project.project.project_id}"
 }
